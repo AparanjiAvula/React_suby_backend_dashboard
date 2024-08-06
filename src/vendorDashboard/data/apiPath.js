0fa878030989be43585d8ec5bbc06daba0810198
AparanjiAvula <avulaaparanji@gmail.com>
2024-08-07 05:28:04 +0530

    changed path to live in api_url

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,3 @@
-//export const api_url='https://backendsuby.onrender.com';
+export const api_url='https://backendsuby.onrender.com';
 
-export const api_url='http://localhost:4000'
+// export const api_url='http://localhost:4000'
